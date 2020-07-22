a13ff1bfeede1899819cb11bc03fea4083c79fc2
Rodrigo Cândido da Silva <rodrigo_candido.dev@outlook.com>
2020-07-22 11:43:20 -0300

    issue solved, wrong field name

--- backend/src/database/migrations/20200722112654_create_scholarities.js
+++ backend/src/database/migrations/20200722112654_create_scholarities.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('scholarities', function(table){
         table.increments('id').primary();
-        table.string('schorality').notNullable();
+        table.string('scholarity').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
